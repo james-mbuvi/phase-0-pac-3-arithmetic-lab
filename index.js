a21418c168c4cb53e655528230015381b560639f
JAMES MBUVI <completed assignment>
2023-12-22 23:06:14 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Function to subtract b from a
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Function to multiply two numbers
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  // Function to divide a by b
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  // Function to increment a number
+  function increment(n) {
+    return n + 1;
+  }
+  
+  // Function to decrement a number
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  // Function to parse n as an integer
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  // Function to parse n as a floating-point number
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
